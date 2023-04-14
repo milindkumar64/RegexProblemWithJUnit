b7aca081222cb0fc91f3fce35742aee2e6bfed64
milindkumar64 <milindgupta@gmail.com>
2023-04-14 12:57:39 +0530

    Add: Atleast One Digit with one Upper Case

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -10,38 +10,36 @@ public class UserRegistration {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter First Name: (mimmum three character that starts with upper case) ");
-		String firstName = sc.next();
-		System.out.println("Enter Last Name: (mimmum three character that starts with upper case) ");
-		String lastName = sc.next();
+		
+		
+		  System.out.
+		  println("Enter First Name: (mimmum three character that starts with upper case) "
+		  ); String firstName = sc.next(); System.out.
+		  println("Enter Last Name: (mimmum three character that starts with upper case) "
+		  ); String lastName = sc.next();
+		  
+		  Pattern p = Pattern.compile("^[A-Z][a-z]{2,}$");
+		  
+		  Matcher mFirst = p.matcher(firstName); Matcher mLast = p.matcher(lastName);
+		  Boolean result1 = mFirst.matches(); Boolean result2 = mLast.matches(); if
+		  ((result1 && result2) == false) System.out.println("Name is Invalid");
+		  
+		  System.out.println("Enter Email : "); String email = sc.next(); Pattern
+		  emailPattern = Pattern.compile("^abc[.][a-z]{3,}@bl[.]co[.][a-z]*$"); Matcher
+		  emailMatcher = emailPattern.matcher(email);
+		  
+		  if (emailMatcher.matches() == false)
+		  System.out.println("Invalid email Smaple: abc.xyz@bl.co.in");
+		  
+		  System.out.println("Enter Mobile number : "); String mob = sc.next(); Pattern
+		  mobPattern = Pattern.compile("^91\s[0-9]{10}$"); Matcher mobMatcher =
+		  mobPattern.matcher(mob); if (mobMatcher.matches() == false)
+		  System.out.println("Invalid MobNumber");
+		 
 
-		Pattern p = Pattern.compile("^[A-Z][a-z]{2,}$");
-
-		Matcher mFirst = p.matcher(firstName);
-		Matcher mLast = p.matcher(lastName);
-		Boolean result1 = mFirst.matches();
-		Boolean result2 = mLast.matches();
-		if ((result1 && result2) == false)
-			System.out.println("Name is Invalid");
-
-		System.out.println("Enter Email : ");
-		String email = sc.next();
-		Pattern emailPattern = Pattern.compile("^abc[.][a-z]{3,}@bl[.]co[.][a-z]*$");
-		Matcher emailMatcher = emailPattern.matcher(email);
-
-		if (emailMatcher.matches() == false)
-			System.out.println("Invalid email Smaple: abc.xyz@bl.co.in");
-
-		System.out.println("Enter Mobile number : ");
-		String mob = sc.next();
-		Pattern mobPattern = Pattern.compile("^91\\s[0-9]{10}$");
-		Matcher mobMatcher = mobPattern.matcher(mob);
-		if (mobMatcher.matches() == false)
-			System.out.println("Invalid MobNumber");
-
-		System.out.println("Enter Password must contain 8 char with atleast one Upper case: ");
+		System.out.println("Enter Password must contain 8 char with atleast digit with one Upper case: ");
 		String pass = sc.next();
-		Pattern passPattern = Pattern.compile("^(?=.*[A-Z]).{8,}$");
+		Pattern passPattern = Pattern.compile("^(?=.*[A-Z])(?=.*[0-9]).{8,}$");
 		Matcher passMatcher = passPattern.matcher(pass);
 		if (passMatcher.matches() == false)
 			System.out.println("Invalid PassWord");
