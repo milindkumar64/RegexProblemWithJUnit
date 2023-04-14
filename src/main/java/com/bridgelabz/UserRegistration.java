80f75fef3764fcfedaa6437f7c5ef85812388a10
milindkumar64 <milindgupta@gmail.com>
2023-04-14 10:59:33 +0530

    Add: validate Email

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -13,16 +13,26 @@ public class UserRegistration {
 		String firstName = sc.next();
 		System.out.println("Enter Last Name: (mimmum three character that starts with upper case) ");
 		String lastName = sc.next();
-		
+
 		Pattern p = Pattern.compile("^[A-Z][a-z]{2,}$");
-		
+
 		Matcher mFirst = p.matcher(firstName);
 		Matcher mLast = p.matcher(lastName);
 		Boolean result1 = mFirst.matches();
 		Boolean result2 = mLast.matches();
-		if (result1 && result2) {
-			System.out.println("Name is valid");
-		} else
+		if ((result1 && result2) == false)
 			System.out.println("Name is Invalid");
+
+		System.out.println("Enter Email : ");
+		String email = sc.next();
+		Pattern emailPattern = Pattern.compile("^abc[.][a-z]{3,}@bl[.]co[.][a-z]*$");
+		Matcher emailMatcher = emailPattern.matcher(email);
+
+		if (emailMatcher.matches())
+			System.out.println("Valid email");
+		else
+			System.out.println("Invalid email Smaple: abc.xyz@bl.co.in");
+
 	}
+
 }
